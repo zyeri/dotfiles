48ad0d837182b0afcf9eaa430af65fdb9eb9eacd
Alexander Schmidt <zyeryi@gmail.com>
2016-10-11 13:39:34 -0500

    Rearranged and updated xmonad.hs, xmobarrc
    My XMonad configuration now uses folds to categorize everything by
    section, hopefully making it easier to navigate. Additionally, I've
    updated it quite a lot.
    
    XMonad updates include
     - ThreeCol layout
     - systemctl suspend binding
     - renamed layouts in the logHook so they take less space on the bar
     - startupHook to launch certain applications (compositor, notification
       daemon, etc.) when starting XMonad
     - probably more stuff I forgot about; I should start making notes on
       changes I make for commit messages...
    
    xmobar changes are pretty simple: I change the battery command so it
    doesn't use the silly static battery icon and instead now indicates that
    it's the battery charge level by saying "batt". Wow, real clever, right?
    Yeah, that's what I thought too. I'll eventually configure it to use an
    icon, changing which one is used specifically based on the current
    charge level, but for now this works well enough so that I know that my
    laptop isn't going to suddenly shut down because my battery ran out of
    charge.
    
    I've also added colors to the battery percentage to provide a visual
    indication of the current charge level; I might tweak the exact colors
    in the future, but I think it should be fine as-is.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,9 +1,11 @@
--- xmonad.hs
+-- {{{ imports
+
 import System.IO
 import System.Exit
 
 import XMonad
 import XMonad.Core
+import XMonad.Config
 
 import XMonad.Layout.Spacing
 import XMonad.Layout.Grid
@@ -11,102 +13,84 @@ import XMonad.Layout.NoBorders
 import XMonad.Layout.IM
 import XMonad.Layout.PerWorkspace
 import XMonad.Layout.SimplestFloat
+import XMonad.Layout.ThreeColumns
 
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.DynamicBars
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.SetWMName
 import XMonad.Hooks.UrgencyHook
 import XMonad.Hooks.Place
 
 import XMonad.Actions.CycleWS
 import XMonad.Actions.GridSelect
 
-import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.Loggers
+import XMonad.Util.Run
 import XMonad.Util.EZConfig(additionalKeysP)
+import XMonad.Util.SpawnOnce
 
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 
--- config
+-- }}}
+-- {{{ options
+
+-- {{{ core stuff
+
 myTerminal              = "/usr/bin/termite"
-
 myModMask               = mod4Mask
-
-myBorderWidth           = 2
-
+myBorderWidth           = 1
 myNormalBorderColor     = "#282A2E"
-
 myFocusedBorderColor    = "#373B41"
+myFocusFollowsMouse     = False
 
+-- clickable workspaces (xmobar)
 myWorkspaces = clickable $ map show [1..9]
     where clickable l = [ "<action=`xdotool key super+" ++ show (n) ++ "`>" ++ ws ++ "</action>" | (i,ws) <- zip [1..9] l, let n = i ]
 
--- layouts
-myLayouts = tiled ||| Mirror tiled ||| Grid ||| Full
+
+-- }}}
+-- {{{ layouts
+
+myLayouts = tiled |||
+            Mirror tiled |||
+            threecol |||
+            Grid |||
+            Full
     where
         -- default tiling algorithm partitions the screen into two panes
         tiled = Tall nmaster delta ratio
 
-        -- The default number of windows in the master pane
+        threecol = ThreeColMid nmaster delta ratio
+
+        -- the default number of windows in the master pane
         nmaster = 1
 
-        -- Default proportion of the screen occupied by the master pane
-        ratio = 1/2
+        -- percent of the screen occupied by the master pane (60%)
+        ratio = 3/5
 
-        -- Percent of the screen to increment by when resizing panes
-        delta = 2/100
+        -- percent of the screen to increment by when resizing panes
+        delta = 3/100
 
 -- layout to use for the workspace containing chat clients
-imLayout = withIM (25/100) (ClassName "telegram-desktop") Grid
+imLayout = withIM (30/100) (ClassName "telegram-desktop") Grid
 
--- combine everything together to make the layoutHook
-myLayoutHook = avoidStruts 
-             $ smartSpacing 10 
-             $ smartBorders 
-             $ onWorkspace (myWorkspaces !! 7) imLayout 
-             $ myLayouts 
+-- }}}
+-- {{{ gridselect configuration
 
--- TODO: Make this section a little (and by a little I mean a lot) neater
-myManageHook = (composeAll . concat $
-    [ [ manageHook defaultConfig
-      , className =? "chromium"           --> doShift (myWorkspaces !! 0)
-      , className =? "google-chrome"      --> doShift (myWorkspaces !! 0)
-      , className =? "qutebrowser"        --> doShift (myWorkspaces !! 0)
-      , className =? "emacs"              --> doShift (myWorkspaces !! 2)
-      , className =? "gvim"               --> doShift (myWorkspaces !! 2)
-      , className =? "telegram-desktop"   --> doShift (myWorkspaces !! 7)
-      , className =? "Slack"              --> doShift (myWorkspaces !! 7)
-      , className =? "discord"            --> doShift (myWorkspaces !! 6)
-      ]
-    , [ className =? c --> doFullFloat | c <- myFullFloats ]
-
-    -- Google Chrome app windows
-    , [ title =? t      --> doFloat | t <- myChromeApps ]
-
-    -- misc.
-    , [ isFullscreen                      --> doFullFloat
-      , isDialog                          --> doCenterFloat
-      , manageDocks
-      ]
-    ]) 
-        where 
-            myFullFloats = [ "mpv" , "Vlc" ]
-            myChromeApps = [ "Authy" ]
-
--- GridSelect configuration
-myGSConfig = defaultGSConfig 
-    { gs_cellheight     = 128 
+myGSConfig = defaultGSConfig
+    { gs_cellheight     = 128
     , gs_cellwidth      = 256
     , gs_cellpadding    = 5
     }
 
--- TODO: I'd eventually like to use lemonbar and/or dzen2 instead of
--- xmobar so it can be more easily configured and customized.
-myLogHook h = dynamicLogWithPP $ xmobarPP
-    { ppTitle       = xmobarColor "#c5c8c6" "" . shorten 80
-    , ppOutput      = hPutStrLn h
-    }
+-- }}}
+
+-- }}}
+-- {{{ keybinds
 
 myAdditionalKeys =
     [ ("<XF86MonBrightnessUp>",     spawn "light -A 10")
@@ -118,31 +102,112 @@ myAdditionalKeys =
     , ("<XF86AudioNext>",           spawn "mpc next")
     , ("<XF86AudioPrev>",           spawn "mpc prev")
     , ("<XF86AudioStop>",           spawn "mpc stop")
-    , ("M-p",                       spawn "dmenu_run -fn 'Roboto Mono-10:medium' -o .85 -q -i -h 20")
+    , ("<XF86Sleep>",               spawn "systemctl suspend")
+    , ("M-p",                       spawn "dmenu_run -fn 'Roboto Mono-10' -o .85 -q -i -h 20")
     , ("M-b",                       sendMessage ToggleStruts)
     , ("M-]",                       nextWS)
     , ("M-[",                       prevWS)
     , ("M-g",                       goToSelected $ myGSConfig)
     ]
 
+-- }}}
+-- {{{ hooks
+
+-- {{{ manage hook
+
+myManageHook = (composeAll . concat $
+    [ [ className =? "chromium"             --> doShift (myWorkspaces !! 0)
+      , className =? "google-chrome"        --> doShift (myWorkspaces !! 0)
+      , className =? "qutebrowser"          --> doShift (myWorkspaces !! 0)
+      , className =? "emacs"                --> doShift (myWorkspaces !! 2)
+      , className =? "gvim"                 --> doShift (myWorkspaces !! 2)
+      , className =? "telegram-desktop"     --> doShift (myWorkspaces !! 7)
+      , className =? "Slack"                --> doShift (myWorkspaces !! 7)
+      , className =? "discord"              --> doShift (myWorkspaces !! 7)
+      , className =? "VirtualBox"           --> doCenterFloat
+      , resource  =? "stalonetray"          --> doIgnore
+      ]
+    , [ className =? c --> doFullFloat | c <- myFullFloats ]
+
+    -- Google Chrome app windows
+    , [ title =? t      --> doFloat | t <- myChromeApps ]
+
+    -- misc.
+    , [ isFullscreen                      --> doFullFloat
+      , isDialog                          --> doCenterFloat
+      , manageDocks
+      ]
+    ])
+        where
+            myFullFloats = [ "mpv" , "Vlc" ]
+            myChromeApps = [ "Authy" ]
+
+-- }}}
+-- {{{ log hook
+
+myLogHook h = dynamicLogWithPP $ xmobarPP
+    { ppTitle       = xmobarColor "#c5c8c6" "" . shorten 80
+    , ppOutput      = hPutStrLn h
+    , ppLayout      = (\layout -> case layout of 
+                            "SmartSpacing 5 Tall"           -> "Tall"
+                            "SmartSpacing 5 Mirror Tall"    -> "Mirror Tall"
+                            "SmartSpacing 5 ThreeCol"       -> "ThreeCol"
+                            "SmartSpacing 5 Grid"           -> "Grid"
+                            "SmartSpacing 5 Full"           -> "Full")
+    }
+
+-- }}}
+-- {{{ startup hook
+
+-- startup applications
+myStartupHook = do
+    -- start dropbox
+    spawnOnce "/usr/bin/dropbox"
+
+    -- start the notification daemon
+    spawnOnce "/usr/bin/dunst"
+
+    -- restore the wallpaper
+    spawnOnce "/usr/bin/nitrogen --restore"
+
+    -- start the compositing engine
+    spawnOnce "/usr/bin/compton --config ~/.config/compton.conf"
+
+-- }}}
+-- {{{ layout hook
+
+-- combine the layouts together to make the layoutHook
+myLayoutHook = avoidStruts 
+             $ onWorkspace (myWorkspaces !! 7) imLayout
+             $ smartSpacing 5
+             $ smartBorders
+             $ myLayouts
+
+-- }}}
+
+-- }}}
+-- {{{ main
 
 main = do
     xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmonad/xmobarrc"
 
-    xmonad $ ewmh defaultConfig
+    xmonad $ withUrgencyHook NoUrgencyHook $ ewmh defaultConfig
         { terminal              = myTerminal
         , modMask               = myModMask
         , borderWidth           = myBorderWidth
         , normalBorderColor     = myNormalBorderColor
         , focusedBorderColor    = myFocusedBorderColor
-
-        -- hooks and layouts
-        , manageHook            = myManageHook
-        , handleEventHook       = docksEventHook <+> handleEventHook defaultConfig
-        , logHook               = myLogHook xmproc
-        , layoutHook            = myLayoutHook
-        , focusFollowsMouse     = False
-
-        -- Custom workspaces
+        , focusFollowsMouse     = myFocusFollowsMouse
         , workspaces            = myWorkspaces
+
+        -- hooks
+        , handleEventHook       = fullscreenEventHook
+        , layoutHook            = myLayoutHook
+        , startupHook           = setWMName "LG3D"  <+> myStartupHook
+        , manageHook            = manageDocks       <+> myManageHook
+        , logHook               = myLogHook xmproc  <+> ewmhDesktopsLogHook
         } `additionalKeysP` myAdditionalKeys
+
+-- }}}
+
+-- vim: foldenable foldmethod=marker
