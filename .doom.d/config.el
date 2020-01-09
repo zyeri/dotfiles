7253ce130f4ec693ed524bce51f0790da0d0d3b9
Alexander Schmidt <zyeryi@gmail.com>
2020-01-09 14:57:42 -0600

    remove unused options, organize a bit
    Corrected broken paths, tried to organize things a bit, and removed some
    things that weren't really doing anything or were redundant because
    doom-emacs does/provides same thing already.

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -4,19 +4,25 @@
       user-mail-address "zyeryi@gmail.com"
       epa-file-encrypt-to user-mail-address)
 
-;; disable automatic completion
-(setq company-idle-delay nil)
-
-;; disable lsp-ui-sideline stuff
-(setq lsp-ui-sideline-enable nil
-      lsp-enable-indentation nil
-      lsp-enable-on-type-formatting nil
-      lsp-enable-symbol-highlighting nil
-      lsp-enable-file-watchers nil)
+;; auto refresh ibuffer
+(add-hook! 'ibuffer-mode-hook #'ibuffer-auto-mode)
 
 ;; disable mouse-overs for mode-line segments
 (setq mode-line-default-help-echo nil
       show-help-function nil)
+
+;; UI
+(setq doom-theme 'doom-one)
+
+;; hide line numbers
+(setq display-line-numbers-type nil)
+
+;; don't yell at us if the fonts aren't installed yet
+(setq doom-font (font-spec :family "Iosevka" :size 14)
+      doom-variable-pitch-font (font-spec :family "Noto Sans" :size 13))
+
+;;; Frames/Windows
+(add-to-list 'default-frame-alist '(inhibit-double-buffering . t))
 
 ;; disable confirmation on quit
 (setq confirm-kill-emacs nil)
@@ -25,34 +31,20 @@
 (setq projectile-project-search-path '("~/projects" "~/src/repos"))
 
 ;; location where Emacs can find its own source code
-(setq source-directory "~/src/repos/emacs/")
+(when (file-directory-p "~/src/repos/emacs")
+  (setq source-directory "~/src/repos/emacs"))
 
 ;; prevent message about iedit during startup (annoying)
 (setq iedit-toggle-key-default nil)
-
-;;
-;;; UI
-(setq doom-theme 'doom-one)
-
-;; line numbers are distracting if we don't need them
-(setq display-line-numbers-type nil)
-
-;; don't yell at us if the fonts aren't installed yet
-(ignore-errors
-  (setq doom-font (font-spec :family "Iosevka" :size 14)
-        doom-variable-pitch-font (font-spec :family "Noto Sans" :size 13)))
-
-;;; Frames/Windows
-(add-to-list 'default-frame-alist '(inhibit-double-buffering . t))
-
-
-
-
 
 ;;; :completion ivy
 (setq +ivy-buffer-preview t)
 
 ;;; :editor evil
+(after! evil
+  ;; don't use 'jk' to escape insert mode
+  (setq evil-escape-key-sequence nil))
+
 (setq evil-split-window-below t
       evil-vsplit-window-right t)
 
@@ -71,15 +63,14 @@
 (setq +vc-gutter-in-remote-files nil)
 
 ;;; :lang latex
-(setq bibtex-completion-library-path (list "~/org/refs/pdfs"))
+(setq bibtex-completion-library-path (list "~/org/bibliography/pdfs"))
 
-(setq +latex-bibtex-file "~/org/refs/references.bib"
+(setq +latex-bibtex-file "~/org/bibliography/references.bib"
       +latex-enable-unicode-math t)
 
-(after! bibtex
-  (setq bibtex-dialect 'biblatex))
+;;; :tools pdf
+(after! pdf-tools
+  (setq-default pdf-view-display-size 'fit-width))
 
-(when IS-MAC
-  (setq +latex-viewers '(skim pdf-tools)))
 (load! "+org")
 (load! "+bindings")
